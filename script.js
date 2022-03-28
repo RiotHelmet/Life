3e48c28e46857dd9ec8243f9fafa5bd1122db481
RiotHelmet <85107883+RiotHelmet@users.noreply.github.com>
2022-03-28 15:13:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -73,8 +73,18 @@ function update() {
   for (var i = 0; i < Pos.length; i++) {
     for (var j = 0; j < Pos[i].length; j++) {
       if (Pos[i][j] === 1) {
-        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "Black";
-      } else {
+        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "#52D017";
+      } else if((Pos[i][j] === 2)) {
+        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "#85BB65";
+      } else if((Pos[i][j] === 3)) {
+        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "#B0BF1A";
+      } else if((Pos[i][j] === 4)) {
+        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "#FFA500";
+      } else if((Pos[i][j] > 4)) {
+        document.getElementById(`[${i}][${j}]`).style.backgroundColor = "#C04000";
+      }
+      
+      else {
         document.getElementById(`[${i}][${j}]`).style.backgroundColor = "White";
       }
     }
@@ -89,43 +99,43 @@ function click(pos) {
 function checkNeighbors(i, j) {
   var count = 0;
   try {
-    if (Pos[i][j - 1] === 1) {
+    if (Pos[i][j - 1] > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i][j + 1] === 1) {
+    if (Pos[i][j + 1] > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i - 1][j] === 1) {
+    if (Pos[i - 1][j] > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i + 1][j] === 1) {
+    if (Pos[i + 1][j] > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i - 1][j - 1] === 1) {
+    if (Pos[i - 1][j - 1]  > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i - 1][j + 1] === 1) {
+    if (Pos[i - 1][j + 1] > 0) {
       count = count + 1;
     }
   } catch (err) {}
 
   try {
-    if (Pos[i + 1][j - 1] === 1) {
+    if (Pos[i + 1][j - 1] > 0) {
       count = count + 1;
     }
   } catch (err) {}
   try {
-    if (Pos[i + 1][j + 1] === 1) {
+    if (Pos[i + 1][j + 1] > 0) {
       count = count + 1;
     }
   } catch (err) {}
@@ -137,6 +147,8 @@ function newGen() {
   var deleteListY = [];
   var birthListX = [];
   var birthListY = [];
+  var surviveListX = []
+  var surviveListY = []
   for (var i = 0; i < Pos.length; i++) {
     for (var j = 0; j < Pos[i].length; j++) {
       if (Pos[i][j] === 0) {
@@ -151,6 +163,10 @@ function newGen() {
         deleteListX.push(i);
         deleteListY.push(j);
       }
+      else {
+        surviveListX.push(i)
+        surviveListY.push(j)
+      }
     }
   }
   for (var i = 0; i < deleteListX.length; i++) {
@@ -162,6 +178,11 @@ function newGen() {
     var x = birthListX[i];
     var y = birthListY[i];
     Pos[x][y] = 1;
+  }
+  for (var i = 0; i < surviveListX.length; i++) {
+    var x = surviveListX[i];
+    var y = surviveListY[i];
+    Pos[x][y] += 1;
   }
   update();
 }
